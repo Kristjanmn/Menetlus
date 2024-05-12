fbcbdbb29d73a2bb6717af7b1866e22a557e4558
Kristjanmn <kristjanmn@hotmail.com>
2024-05-12 13:38:40 +0300

    Create ApplicationProperties

---
+++ backend/src/main/java/io/nqa/menetlus/configuration/ApplicationProperties.java
@@ -0,0 +1,25 @@
+package io.nqa.menetlus.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+    private RabbitMq rabbitMq;
+
+    @Data
+    public static class RabbitMq {
+        private boolean durable;
+        private String queueName;
+        private String topicExchangeName;
+        private String headerExchangeName;
+        private String directExchangeName;
+        private String fanoutExchangeName;
+        private String headerArgument;
+        private String routingKey;
+        private String directRoutingKey;
+    }
+}

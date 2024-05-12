a1af0b2f8dd3bb143e2a76329643772de6034516
Kristjanmn <kristjanmn@hotmail.com>
2024-05-12 22:02:56 +0300

    Update Menetlus entity

--- backend/src/main/java/io/nqa/menetlus/entity/Menetlus.java
+++ backend/src/main/java/io/nqa/menetlus/entity/Menetlus.java
@@ -1,17 +1,22 @@
 package io.nqa.menetlus.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "menetlus")
 public class Menetlus {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private long isikukood;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private boolean emailDelivered;
+    @Column(nullable = false)
     private String reason;
 }

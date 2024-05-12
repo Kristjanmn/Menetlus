b3b01c25b314f5b2183862314572f784dbf25c4c
Kristjanmn <kristjanmn@hotmail.com>
2024-05-12 22:34:44 +0300

    Update MenetlusService

--- backend/src/main/java/io/nqa/menetlus/service/IMenetlusService.java
+++ backend/src/main/java/io/nqa/menetlus/service/IMenetlusService.java
@@ -1,4 +1,14 @@
 package io.nqa.menetlus.service;
 
+import io.nqa.menetlus.entity.Menetlus;
+import io.nqa.menetlus.model.MenetlusDTO;
+
+import java.util.List;
+
 public interface IMenetlusService {
+    List<Menetlus> getAll();
+    List<MenetlusDTO> getAllDtos();
+    Menetlus getById(final long id);
+    MenetlusDTO getDtoById(final long id);
+    void save(final Menetlus menetlus);
 }
--- backend/src/main/java/io/nqa/menetlus/service/MenetlusService.java
+++ backend/src/main/java/io/nqa/menetlus/service/MenetlusService.java
@@ -1,7 +1,52 @@
 package io.nqa.menetlus.service;
 
+import io.nqa.menetlus.entity.Menetlus;
+import io.nqa.menetlus.model.MenetlusDTO;
+import io.nqa.menetlus.repository.MenetlusRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class MenetlusService implements IMenetlusService {
+    private final MenetlusRepository repository;
+    private final ModelMapper modelMapper = new ModelMapper();
+
+    @Override
+    public List<Menetlus> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public List<MenetlusDTO> getAllDtos() {
+        List<Menetlus> menetlusList = this.getAll();
+        List<MenetlusDTO> dtoList = new ArrayList<>();
+        for (Menetlus menetlus : menetlusList) {
+            dtoList.add(modelMapper.map(menetlus, MenetlusDTO.class));
+        }
+        return dtoList;
+    }
+
+    @Override
+    public Menetlus getById(long id) {
+        Optional<Menetlus> menetlus = repository.findById(id);
+        return menetlus.orElse(null);
+    }
+
+    @Override
+    public MenetlusDTO getDtoById(long id) {
+        Menetlus menetlus = this.getById(id);
+        if (menetlus == null) return null;
+        return modelMapper.map(menetlus, MenetlusDTO.class);
+    }
+
+    @Override
+    public void save(Menetlus menetlus) {
+        this.repository.save(menetlus);
+    }
 }

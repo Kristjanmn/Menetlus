1d0e78231848da77dd96e02ddaaa3ad81a15c15e
Kristjanmn <kristjanmn@hotmail.com>
2024-05-12 13:38:55 +0300

    Create RabbitMqConfig

---
+++ backend/src/main/java/io/nqa/menetlus/configuration/RabbitMqConfig.java
@@ -0,0 +1,57 @@
+package io.nqa.menetlus.configuration;
+
+import lombok.Data;
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+public class RabbitMqConfig {
+    private final ApplicationProperties.RabbitMq properties;
+
+    @Bean
+    Queue queue() {
+        return new Queue(properties.getQueueName(), properties.isDurable());
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(properties.getTopicExchangeName());
+    }
+
+    @Bean
+    HeadersExchange headers() {
+        return new HeadersExchange(properties.getHeaderExchangeName());
+    }
+
+    @Bean
+    DirectExchange directExchange() {
+        return new DirectExchange(properties.getDirectExchangeName());
+    }
+
+    @Bean
+    FanoutExchange fanoutExchange() {
+        return new FanoutExchange(properties.getFanoutExchangeName());
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(properties.getRoutingKey());
+    }
+
+    @Bean
+    Binding bindingHeader(Queue queue, HeadersExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).where(properties.getHeaderArgument()).exists();
+    }
+
+    @Bean
+    Binding directExchangeBinding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(properties.getDirectRoutingKey());
+    }
+
+    @Bean
+    Binding bindingFanout(Queue queue, FanoutExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange);
+    }
+}

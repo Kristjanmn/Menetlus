bf3fe80d6c2245f5d5edc25fe0c643844226d421
Kristjanmn <kristjanmn@hotmail.com>
2024-05-15 23:45:55 +0300

    Update appConfig

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { AppModule } from './app.module';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    AppModule,
+    provideRouter(routes),
+    provideHttpClient()
+  ]
 };

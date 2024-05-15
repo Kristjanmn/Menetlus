947fe3e43dbb393e75e815c4cb6beb54926368b4
Kristjanmn <kristjanmn@hotmail.com>
2024-05-15 23:52:57 +0300

    Update frontend routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MenetlusTableComponent } from './components/menetlus/menetlus-table/menetlus-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: MenetlusTableComponent}
+];

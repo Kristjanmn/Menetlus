fac6507bfbf0c71a41d6f79204a6de3a1a92ea6b
Kristjanmn <kristjanmn@hotmail.com>
2024-05-17 02:22:02 +0300

    Update Menetlus object (frontend)

--- frontend/src/app/model/menetlus.ts
+++ frontend/src/app/model/menetlus.ts
@@ -1,8 +1,20 @@
-export interface Menetlus {
-  id: number;
+export class Menetlus {
+  id: number = 0;
   name: string;
   personalCode: number;
   email: string;
-  emailDelivered: boolean;
+  emailDelivered: boolean = false;
   reason: string;
+
+  constructor(
+    name: string,
+    personalCode: number,
+    email: string,
+    reason: string
+  ) {
+    this.name = name;
+    this.personalCode = personalCode;
+    this.email = email;
+    this.reason = reason;
+  }
 }

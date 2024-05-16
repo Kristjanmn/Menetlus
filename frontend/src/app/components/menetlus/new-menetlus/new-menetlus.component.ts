a5fdf1dc286b451913326747cf8f9b77ec28b424
Kristjanmn <kristjanmn@hotmail.com>
2024-05-17 02:33:22 +0300

    Create NewMenetlusComponent

---
+++ frontend/src/app/components/menetlus/new-menetlus/new-menetlus.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Menetlus } from '../../../model/menetlus';
+import { MenetlusService } from '../../../service/menetlus.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'new-menetlus',
+  templateUrl: './new-menetlus.component.html'
+})
+export class NewMenetlusComponent {
+  menetlus: Menetlus = new Menetlus('', 0, '', '');
+
+  constructor(
+    private service: MenetlusService,
+    private router: Router
+  ) {
+  }
+
+  onSubmit() {
+    if (this.service.validateForm(this.menetlus)) {
+      this.service.saveMenetlus(this.menetlus)
+        .subscribe(response => {
+          if (response.success) this.goBack();
+        }).unsubscribe();
+    }
+  }
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
+}

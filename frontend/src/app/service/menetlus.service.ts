784efa5631f5ad41904860b4d3d1073155177715
Kristjanmn <kristjanmn@hotmail.com>
2024-05-17 02:32:22 +0300

    Update MenetlusService (frontend)

--- frontend/src/app/service/menetlus.service.ts
+++ frontend/src/app/service/menetlus.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { Menetlus } from '../model/menetlus';
+import { CustomResponse } from '../model/custom-response';
 
 @Injectable({
   providedIn: "root"
@@ -12,15 +13,36 @@ export class MenetlusService {
 
   constructor(private http: HttpClient) {}
 
-  getAll(): Observable<Menetlus[]> {
-    return this.http.get<Menetlus[]>(this.url);
+  getAll(): Observable<CustomResponse> {
+    return this.http.get<CustomResponse>(this.url);
   }
 
-  getById(id: number): Observable<Menetlus> {
-    return this.http.get<Menetlus>(`this.url/${id}`);
+  getById(id: number): Observable<CustomResponse> {
+    return this.http.get<CustomResponse>(`this.url/${id}`);
   }
 
-  newMenetlus(menetlus: Menetlus): void {
-    this.http.post(this.url, menetlus);
+  saveMenetlus(menetlus: Menetlus): Observable<CustomResponse> {
+    return this.http.post<CustomResponse>(this.url, menetlus);
+  }
+
+  validateForm(menetlus: Menetlus): boolean {
+    let emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+    if (menetlus.name.trim().length < 1) {
+      alert("Isiku nimi on kohustuslik!");
+      return false;
+    }
+    if (menetlus.personalCode == null) {
+      alert("Isikukoodi väli on tühi või sisaldab tähemärki! Ainult numbrid on lubatud!")
+      return false;
+    }
+    if (menetlus.personalCode.toString().length !== 11) {
+      alert("Isikukood peab olema 11 numbrit!");
+      return false;
+    }
+    if (!emailRegex.test(menetlus.email)) {
+      alert("E-kirja formaat on vale!");
+      return false;
+    }
+    return true;
   }
 }

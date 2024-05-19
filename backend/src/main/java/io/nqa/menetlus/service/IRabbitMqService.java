0d7a41dec599900c5af6634e8a4e25204990603c
Kristjanmn <kristjanmn@hotmail.com>
2024-05-20 01:43:55 +0300

    Remove RabbitMqService (backend), required functions are in MenetlusService

--- backend/src/main/java/io/nqa/menetlus/service/IRabbitMqService.java
+++
@@ -1,8 +0,0 @@
-package io.nqa.menetlus.service;
-
-public interface IRabbitMqService {
-    void sendViaTopicExchange(String message);
-    void sendViaHeadersExchange(String message);
-    void sendViaDirectExchange(String message);
-    void sendViaFanoutExchange(String message);
-}

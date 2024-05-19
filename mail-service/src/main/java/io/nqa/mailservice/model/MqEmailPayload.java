dd2c6f1a03a2a360e08e803291fc05b524276abd
Kristjanmn <kristjanmn@hotmail.com>
2024-05-20 01:23:28 +0300

    Create MqEmailPayload for rabbitmq communication (backend <-> mail-service)

---
+++ mail-service/src/main/java/io/nqa/mailservice/model/MqEmailPayload.java
@@ -0,0 +1,16 @@
+package io.nqa.mailservice.model;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class MqEmailPayload implements Serializable {
+    private long id;
+    private String email;
+    private boolean sent;
+}

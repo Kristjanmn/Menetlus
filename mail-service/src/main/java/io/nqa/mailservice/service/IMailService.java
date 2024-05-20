bdfd018cb36699abe9d793e39d0bb757def63839
Kristjanmn <kristjanmn@hotmail.com>
2024-05-20 11:12:58 +0300

    Update MailService documentation

--- mail-service/src/main/java/io/nqa/mailservice/service/IMailService.java
+++ mail-service/src/main/java/io/nqa/mailservice/service/IMailService.java
@@ -1,6 +1,24 @@
 package io.nqa.mailservice.service;
 
 public interface IMailService {
+
+    /**
+     * Send an email.
+     *
+     * @param to Recipiant's e-mail address
+     * @param subject E-mail subject
+     * @param text E-mail content
+     * @return Returns true if sending was successful
+     * or if mail service is disabled in config
+     */
     boolean send(String to, String subject, String text);
+
+    /**
+     * Send pre-made e-mail.
+     *
+     * @param to Recipiant's e-mail address
+     * @return Returns true if sending was successful
+     * or if mail service is disabled in config
+     */
     boolean sendMenetlusEmail(String to);
 }
